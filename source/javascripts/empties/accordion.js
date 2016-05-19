85cbbc8f9eb3e8ff225d1c6ba36bc32f4476229d
Eric Nolte <Eric.Nolte@markit.com>
2016-05-19 08:57:19 -0600

    Add roles and aria attributes to accordions

--- source/javascripts/empties/accordion.js
+++ source/javascripts/empties/accordion.js
@@ -1,5 +1,10 @@
 $('.js-accordion-trigger').bind('click', function(e){
-  jQuery(this).parent().find('.submenu').slideToggle('fast');  // apply the toggle to the ul
+  var $submenu = jQuery(this).parent().find('.submenu');
+  $submenu.slideToggle('fast');  // apply the toggle to the ul
+  // toggle aria attributes
+  $submenu.attr('aria-hidden') == 'true' ? 
+	 $submenu.attr('aria-hidden', 'false') :
+	 $submenu.attr('aria-hidden', 'true');
   jQuery(this).parent().toggleClass('is-expanded');
   e.preventDefault();
 });
